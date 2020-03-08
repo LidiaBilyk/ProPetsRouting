b418f7a01f19a96a49934b93bd14fbb6ff4bf680
bilyk <bilyk@10.0.0.21>
2020-03-08 17:30:35 +0200

    First commit.

---
+++ ProPetsRouting/src/main/java/telran/propets/ProPetsRoutingApplication.java
@@ -0,0 +1,15 @@
+package telran.propets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ProPetsRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsRoutingApplication.class, args);
+	}
+
+}
